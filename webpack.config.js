f92f4f0f2b8cb50871015c5f437e23797e8c5fb9
Estevan Maito <ejmaito@gmail.com>
2020-05-12 12:45:19 -0300

    fix async run

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "lib"),
-    filename: "billie.js",
-    library: "billie",
-    libraryTarget: "umd"
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'billie.js',
+    library: 'billie',
+    libraryTarget: 'umd',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  }
-};
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
